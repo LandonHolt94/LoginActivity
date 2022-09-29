f633a5e17c756bb26aa7375f7aed4d9efe07342c
LandonHolt94 <111383436+LandonHolt94@users.noreply.github.com>
2022-09-28 18:06:17 -0700

    Login Activity App

--- app/src/main/java/com/example/loginactivity/MainActivity.java
+++ app/src/main/java/com/example/loginactivity/MainActivity.java
@@ -17,21 +17,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+// making sure that your Identities that are to be used are labeled in your main activity.
         TextView username = (TextView) findViewById(R.id.username);
         TextView password = (TextView) findViewById(R.id.password);
 
         Button loginbtn = (MaterialButton) findViewById(R.id.loginbtn);
 
         //admin and admin
+//This is the code to execute the login
 
         loginbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //This is what will determine if login is successful or not.
                 if (username.getText().toString().equals("admin") && password.getText().toString().equals("admin")) {
                     //correct
+                    //We use Toast to display if login was successful
                     Toast.makeText(MainActivity.this, "LOGIN SUCCESSFUL",Toast.LENGTH_SHORT).show();
                 }else
+                    //We use Toast to display if login was unsuccessful
                     Toast.makeText(MainActivity.this, "LOGIN FAILED",Toast.LENGTH_SHORT).show();
             }
         });
